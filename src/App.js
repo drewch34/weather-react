7fb81ddbcaba49b76c9aa1f24e50c90e8178f2b1
Drew <firefan34@gmail.com>
2020-12-15 15:58:41 -0500

    add glass effect background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Search from "./Search";
 
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Search />
-      <p>
-        <a href="https://github.com/drewch34/weather-react.git">Open-Source Code</a> by Drew
-      </p>
+      <div className="container">
+        <Search />
+
+      </div>
     </div>
   );
 
